a5a3f766e15e2d0be53201e20608f8d3d9a5bad0
stevealbertwong <wongkwunkit@gmail.com>
2018-01-01 02:42:29 -0800

    same seg fault

--- proxy.cpp
+++ proxy.cpp
@@ -61,7 +61,7 @@ void HTTPProxy::ProxyRequest(){
     
     if(ParsedRequest_parse(req, request_message, strlen(request_message))<0){
         cout << "request message format not supported yet" << endl;
-        break;
+        return 0;
     }
 
     if (req->port == NULL) {
