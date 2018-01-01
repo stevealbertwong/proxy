2a5a1a6bd83879be6f35c006bbee109e31965f17
stevealbertwong <wongkwunkit@gmail.com>
2018-01-01 00:21:24 -0800

    cout debug

--- proxy.cpp
+++ proxy.cpp
@@ -99,7 +99,8 @@ void HTTPProxy::ProxyBackClient(int client_fd, int remote_socket){
 
 void HTTPProxy::SendRequestRemote(const char *req_string, int remote_socket, int buff_length){
 	int totalsent = 0;
-	int senteach;
+    int senteach;
+    cout << "SendRequestRemote : "<< totalsent <<buff_length << endl;
 	while (totalsent < buff_length) {
 		if ((senteach = send(remote_socket, (void *) (req_string + totalsent), buff_length - totalsent, 0)) < 0) {
 			fprintf (stderr," Error in sending to server ! \n");
